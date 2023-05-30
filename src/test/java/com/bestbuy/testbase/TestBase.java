fe94c04390f7a748d29d693baf417564e741c869
Maulin-Patel <sareejunction@gmail.com>
2023-05-30 12:46:53 +0100

    Week 17 Bestbuy RestAssured

---
+++ src/test/java/com/bestbuy/testbase/TestBase.java
@@ -0,0 +1,12 @@
+package com.bestbuy.testbase;
+
+public class TestBase {
+
+//
+//    @BeforeClass
+//    public static void inIt() {
+//        RestAssured.baseURI = "http://localhost";
+//        RestAssured.port = 3030;
+//    }
+}
+
